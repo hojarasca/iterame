746fc28f21dab1c9a312188e07c486703c42f6df
Migue <miguelfeliped@gmail.com>
2024-01-31 23:48:07 -0300

    added defaults to package.json

---
+++ src/pure.ts
@@ -0,0 +1,6 @@
+export * from './iterators/array-iterator.js'
+export * from "./operations/iter-map.js"
+export * from './iterator.js'
+export * from './operations/index.js'
+export * from './collectors/index.js'
+export * from './iterators/one-piece-iterator.js'
